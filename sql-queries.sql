19e94f87615b79062817ef0353372e02696b3cf3
Darien Kentanu <darienkentanu@gmail.com>
2021-12-20 01:39:42 +0700

    add sql-queries.sql

---
+++ sql-queries.sql
@@ -0,0 +1,7 @@
+create database if not exists `antikode`;
+use antikode;
+
+select name from brands;
+select name, address, longitude, latitude from outlets;
+select count(id) from products;
+select distance from outlets;
